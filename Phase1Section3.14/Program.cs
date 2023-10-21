33dd81351a12236c2ae60a5fba9144695a40a073
NakkaAnilkumar <anilsunny1999@gmail.com>
2023-10-21 14:48:16 +0530

    Add project files.

---
+++ Phase1Section3.14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+namespace Section6PracticeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Path to the text file
+            string filePath = "C:\\Users\\Admin\\source\\repos\\Phase1Section3.14\\Phase1Section3.14\\Student.txt";
+            // Checks if the file exists
+            if (File.Exists(filePath))
+            {
+                // Reads all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+                // Display the student data
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Student data file not found!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
